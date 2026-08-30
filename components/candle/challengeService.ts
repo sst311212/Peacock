9bb91777df99876bfcadeb38dfa3c798e5fc6c4c
suanjiansalt <127009844+suanjiansalt@users.noreply.github.com>
2026-08-30 22:33:30 +0800

    fix: move ammonia to its appropriate place

--- components/candle/challengeService.ts
+++ components/candle/challengeService.ts
@@ -224,16 +224,6 @@ export abstract class ChallengeRegistry {
             },
         ],
         [
-            "ammonia-pack",
-            {
-                Name: "UI_MENU_PAGE_PROFILE_CHALLENGES_CATEGORY_PACK_AMMONIA",
-                Description: "",
-                GameVersions: ["h3"],
-                Image: "images/challenges/categories/packammonia/tile.jpg",
-                Icon: "challenge_category_feats",
-            },
-        ],
-        [
             "argentum-pack",
             {
                 Name: "UI_MENU_PAGE_PROFILE_CHALLENGES_CATEGORY_PACK_ARGENTUM",
@@ -260,6 +250,16 @@ export abstract class ChallengeRegistry {
                 Description: "",
                 GameVersions: ["h3"],
                 Image: "images/challenges/categories/packcheesecake/tile.jpg",
+                Icon: "challenge_category_feats",
+            },
+        ],
+        [
+            "ammonia-pack",
+            {
+                Name: "UI_MENU_PAGE_PROFILE_CHALLENGES_CATEGORY_PACK_AMMONIA",
+                Description: "",
+                GameVersions: ["h3"],
+                Image: "images/challenges/categories/packammonia/tile.jpg",
                 Icon: "challenge_category_feats",
             },
         ],
